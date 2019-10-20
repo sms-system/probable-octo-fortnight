c6e0c57bc8975abe9f78a4c83d467936b52d5aa7
Serge Smirnov <sms-system@shri2018.yaconnect.com>
2019-10-20 12:01:19 +0300

    Small refactorings

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 module.exports = (arr = []) => {
-  let sortedElementsCount = 0
-  while (sortedElementsCount !== arr.length) {
+  let endPos = arr.length - 1
+  while (endPos !== 0) {
     let maxIndex, maxValue
-    for (let i=0; i<arr.length - sortedElementsCount; i++) {
+    for (let i=0; i<=endPos; i++) {
       if (arr[i] >= maxValue || maxValue === undefined) { maxValue = arr[i]; maxIndex = i }
     }
-    const endPos = arr.length - sortedElementsCount - 1
+
     const tmp = arr[endPos]
     arr[endPos] = arr[maxIndex]
     arr[maxIndex] = tmp
-    sortedElementsCount++
+    endPos--
   }
   return arr
 }
